3a0da511b330180f5e6ef90cca2b2ed7f4f9f77d
Khushicodes15 <khushidhona06@gmail.com>
2026-01-18 16:29:02 +0530

    Frog Jump

---
+++ frogJump3.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<bits/stdc++.h>
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        int prev=0;
+        int prev2=0;
+        for(int i=1;i<n;i++){
+
+            int left=prev +abs(height[i]-height[i-1]);
+            int right=INT_MAX;
+            if(i>1) right=prev2 +abs(height[i]-height[i-2]);
+            
+            int curri=min(left,right);
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+        
+    }
+};
